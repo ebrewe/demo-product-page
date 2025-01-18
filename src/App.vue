3c649f91613c6d042e5b9f7eb125a9f04e62bef6
Eric Brewer <ericdbrewer@gmail.com>
2025-01-18 09:39:19 -0500

    initial setup

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    this is the header
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+    </nav>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
